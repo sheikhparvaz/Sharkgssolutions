49742a03aabb87dfe995b1f64c25eeb9763fa232
sheikhparvaz <sheikhparvaz@gmail.com>
2026-09-05 20:05:12 +0530

    Program.cs change

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// Explicitly configure console logging
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
@@ -12,7 +16,7 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection(); // <--- COMMENT THIS LINE OUT
 app.UseStaticFiles();
 
 app.UseRouting();
